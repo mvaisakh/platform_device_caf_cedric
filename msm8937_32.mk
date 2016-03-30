6e8cad237506c01d3335bd15ef08df4f0786c96e
Ashish Jain <ashishj@codeaurora.org>
2016-03-30 12:19:59 +0530

    msm8937_32: move audio config files to audio HAL folder.
    Moving audio config files to Audio HAL folder for
    ease of future modification.
    Move all audio specific properties from system.prop
    to audio hal configs folder.
    
    Change-Id: Iaf4b226cecc851c28dfd55c76980ef0ebb46bade

--- msm8937_32.mk
+++ msm8937_32.mk
@@ -51,31 +51,7 @@ ifneq ($(strip $(QCPATH)),)
 endif
 
 # Audio configuration file
-PRODUCT_COPY_FILES += \
-    device/qcom/msm8937_32/audio_policy.conf:system/etc/audio_policy.conf \
-    device/qcom/msm8937_32/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    device/qcom/msm8937_32/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    device/qcom/msm8937_32/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
-    device/qcom/msm8937_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
-    device/qcom/msm8937_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_skum.xml:system/etc/mixer_paths_qrd_skum.xml \
-    device/qcom/msm8937_32/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/qcom/msm8937_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    device/qcom/msm8937_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-    device/qcom/msm8937_32/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
-    device/qcom/msm8937_32/sound_trigger_mixer_paths_wcd9335.xml:system/etc/sound_trigger_mixer_paths_wcd9335.xml \
-    device/qcom/msm8937_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    device/qcom/msm8937_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml \
-    device/qcom/msm8937_32/mixer_paths_wcd9335.xml:system/etc/mixer_paths_wcd9335.xml \
-    device/qcom/msm8937_32/mixer_paths_wcd9326.xml:system/etc/mixer_paths_wcd9326.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_skun.xml:system/etc/mixer_paths_qrd_skun.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_sku1.xml:system/etc/mixer_paths_qrd_sku1.xml \
-    device/qcom/msm8937_32/mixer_paths_qrd_sku2.xml:system/etc/mixer_paths_qrd_sku2.xml \
-    device/qcom/msm8937_32/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    device/qcom/msm8937_32/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml
+-include $(TOPDIR)hardware/qcom/audio/configs/msm8937/msm8937.mk
 
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
@@ -138,11 +114,6 @@ PRODUCT_COPY_FILES += \
 #fstab.qcom
 PRODUCT_PACKAGES += fstab.qcom
 
-PRODUCT_PACKAGES += \
-    libqcomvisualizer \
-    libqcompostprocbundle \
-    libqcomvoiceprocessing
-
 #OEM Services library
 PRODUCT_PACKAGES += oem-services
 PRODUCT_PACKAGES += libsubsystem_control
@@ -187,10 +158,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SUPPORTS_VERITY := true
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
 
-# Reduce client buffer size for fast audio output tracks
-PRODUCT_PROPERTY_OVERRIDES += \
-     af.fast_track_multiplier=1
-
-# Low latency audio buffer size in frames
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio_hal.period_size=192
