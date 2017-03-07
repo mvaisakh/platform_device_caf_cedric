f288b47655b434adb2068ccef6c5210f46504a3d
Bharat Pawar <bpawar@codeaurora.org>
2017-03-07 17:47:04 +0530

    msm8937_32: Initial vintf manifest with audio, omx and graphics HALs
    - Create initial vendor manifest file and enable
      * audio
      * omx media treble
      * graphics with allocator, mapper and composer HALs
    - Enable Lights HAL compilation.
    - Enabled HWC2 and GRALLOC1 flags.
    - Removed duplicate definition.
    
    Change-Id: I9b7077dc66ef02f287fd2b375cc4d1cb22a6e415

--- msm8937_32.mk
+++ msm8937_32.mk
@@ -1,4 +1,5 @@
 TARGET_USES_AOSP := true
+TARGET_USES_AOSP_FOR_AUDIO := true
 TARGET_USES_QCOM_BSP := false
 
 ifeq ($(TARGET_USES_AOSP),true)
@@ -50,6 +51,11 @@ endif
                      com.qti.dpmframework
 #PRODUCT_BOOT_JARS += qcom.fmradio
 #PRODUCT_BOOT_JARS += qcmediaplayer
+
+# add vendor manifest file
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8937_32/vintf.xml:system/vendor/manifest.xml
+
 ifneq ($(strip $(QCPATH)),)
 #    PRODUCT_BOOT_JARS += WfdCommon
     PRODUCT_BOOT_JARS += oem-services
@@ -69,6 +75,17 @@ PRODUCT_PACKAGES += \
     AntHalService \
     libantradio \
     antradio_app
+
+# Gralloc
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl
+
+# HW Composer
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
 
 # Feature definition files for msm8937
 PRODUCT_COPY_FILES += \
@@ -107,11 +124,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
-#ANT+ stack
-PRODUCT_PACKAGES += \
-AntHalService \
-libantradio \
-antradio_app
 
 PRODUCT_PACKAGES += telephony-ext
 #PRODUCT_BOOT_JARS += telephony-ext
@@ -156,3 +168,6 @@ endif
 
 #Keymaster
 PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl
+
+#Enable Lights Impl HAL Compilation
+PRODUCT_PACKAGES += android.hardware.light@2.0-impl
