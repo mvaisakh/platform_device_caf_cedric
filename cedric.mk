4bdcf3d1636b7e4e0c069bf9cf9da3c141a7806b
Vaisakh Murali <vaisakhmurali@gmail.com>
2018-02-16 18:13:03 +0530

    cedric: Clean up.
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cedric.mk
+++ cedric.mk
@@ -1,28 +1,10 @@
-TARGET_USES_AOSP := true
-TARGET_USES_AOSP_FOR_AUDIO := false
 TARGET_USES_QCOM_BSP := false
 
-ifeq ($(TARGET_USES_AOSP),true)
-TARGET_DISABLE_DASH := true
-endif
-
 DEVICE_PACKAGE_OVERLAYS := device/motorola/cedric/overlay
-# Default vendor configuration.
-ifeq ($(ENABLE_VENDOR_IMAGE),)
-ENABLE_VENDOR_IMAGE := true
-endif
-# Disable QTIC until it's brought up in split system/vendor
-# configuration to avoid compilation breakage.
-ifeq ($(ENABLE_VENDOR_IMAGE), true)
-#TARGET_USES_QTIC := false
-endif
 
 BOARD_HAVE_QCOM_FM := true
 
-TARGET_USES_NQ_NFC := true
 TARGET_KERNEL_VERSION := 3.18
-
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
 # Enable features in video HAL that can compile only on this platform
 TARGET_USES_MEDIA_EXTENSIONS := true
@@ -30,7 +12,6 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 -include $(QCPATH)/common/config/qtic-config.mk
 
 # media_profiles and media_codecs xmls for msm8937
-ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += device/motorola/cedric/media/media_profiles_8937.xml:system/etc/media_profiles.xml \
                       device/motorola/cedric/media/media_profiles_8937.xml:system/etc/media_profiles_vendor.xml \
                       device/motorola/cedric/media/media_profiles_8956.xml:system/etc/media_profiles_8956.xml \
@@ -39,29 +20,25 @@ PRODUCT_COPY_FILES += device/motorola/cedric/media/media_profiles_8937.xml:syste
                       device/motorola/cedric/media/media_codecs_8956.xml::system/etc/media_codecs_8956.xml \
                       device/motorola/cedric/media/media_codecs_performance_8937.xml:system/etc/media_codecs_performance.xml \
                       device/motorola/cedric/media/media_codecs_vendor_audio.xml:system/etc/media_codecs_vendor_audio.xml
-endif
+
 # video seccomp policy files
 PRODUCT_COPY_FILES += \
     device/motorola/cedric/seccomp/mediacodec-seccomp.policy:system/etc/seccomp_policy/mediacodec.policy \
     device/motorola/cedric/seccomp/mediaextractor-seccomp.policy:system/etc/seccomp_policy/mediaextractor.policy
-
-PRODUCT_COPY_FILES += device/motorola/cedric/whitelistedapps.xml:system/etc/whitelistedapps.xml \
-                      device/motorola/cedric/gamedwhitelist.xml:system/etc/gamedwhitelist.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.disable.split.mode=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
        dalvik.vm.heapminfree=6m \
-       dalvik.vm.heapstartsize=14m \
+       dalvik.vm.heapstartsize=14m
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, device/qcom/common/common.mk)
 
 PRODUCT_NAME := cedric
 PRODUCT_DEVICE := cedric
 
-#PRODUCT_BOOT_JARS += vcard \
-                     com.qti.dpmframework
 PRODUCT_BOOT_JARS += qcom.fmradio
 
 DEVICE_MANIFEST_FILE := device/motorola/cedric/manifest.xml
@@ -69,10 +46,7 @@ DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 
 ifneq ($(strip $(QCPATH)),)
     PRODUCT_BOOT_JARS += WfdCommon
-    PRODUCT_BOOT_JARS += oem-services
     PRODUCT_BOOT_JARS += tcmiface
-#    PRODUCT_BOOT_JARS += dpmapi
-#    PRODUCT_BOOT_JARS += com.qti.location.sdk
 endif
 
 # Audio configuration file
@@ -127,11 +101,6 @@ PRODUCT_PACKAGES += \
 #fstab.qcom
 PRODUCT_PACKAGES += fstab.qcom
 
-#OEM Services library
-PRODUCT_PACKAGES += oem-services
-PRODUCT_PACKAGES += libsubsystem_control
-PRODUCT_PACKAGES += libSubSystemShutdown
-
 PRODUCT_PACKAGES += wcnss_service
 
 # MSM IRQ Balancer configuration file
@@ -151,7 +120,6 @@ endif
 PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
-
 
 #for wlan
 PRODUCT_PACKAGES += \
@@ -174,10 +142,6 @@ PRODUCT_PACKAGES += android.hardware.health@1.0-impl \
                    android.hardware.health@1.0-convert \
                    android.hardware.health@1.0-service \
                    libhealthd.msm
-
-PRODUCT_FULL_TREBLE_OVERRIDE := true
-
-PRODUCT_VENDOR_MOVE_ENABLED := true
 
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
@@ -204,27 +168,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += camera.device@3.2-impl
 PRODUCT_PACKAGES += camera.device@1.0-impl
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+
 # Enable binderized camera HAL
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
-
-PRODUCT_SUPPORTS_VERITY := true
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
-ifeq ($(ENABLE_VENDOR_IMAGE), true)
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/bootdevice/by-name/vendor
-endif
-# Enable logdumpd service only for non-perf bootimage
-ifeq ($(findstring perf,$(KERNEL_DEFCONFIG)),)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
-        PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=0
-    else
-        #PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=1
-    endif
-else
-    PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-        ro.logdumpd.enabled=0
-endif
 
 PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
@@ -243,21 +189,11 @@ PRODUCT_PACKAGES += android.hardware.thermal@1.0-impl \
 
 TARGET_SUPPORT_SOTER := true
 
-#set KMGK_USE_QTI_SERVICE to true to enable QTI KEYMASTER and GATEKEEPER HIDLs
-ifeq ($(ENABLE_VENDOR_IMAGE), true)
-KMGK_USE_QTI_SERVICE := true
-endif
-
 #Enable AOSP KEYMASTER and GATEKEEPER HIDLs
-ifneq ($(KMGK_USE_QTI_SERVICE), true)
 PRODUCT_PACKAGES += android.hardware.gatekeeper@1.0-impl \
                     android.hardware.gatekeeper@1.0-service \
                     android.hardware.keymaster@3.0-impl \
                     android.hardware.keymaster@3.0-service
-endif
 
 PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so
 
-#Boot control HAL test app
-PRODUCT_PACKAGES_DEBUG += bootctl
-endif
