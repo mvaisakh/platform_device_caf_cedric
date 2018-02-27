20b83831e0ec3fc9b8df774302a420df83873c4f
Vaisakh Murali <vaisakhmurali@gmail.com>
2018-02-27 23:36:47 +0530

    Add the camera configs from NPPS25.137-93-4
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- camera.mk
+++ camera.mk
@@ -3,3 +3,8 @@
 TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_USES_NON_TREBLE_CAMERA := true
+
+PRODUCT_COPY_FILES += device/motorola/cedric/configs/mot_imx258_chromatix.xml:system/etc/camera/mot_imx258_chromatix.xml \
+                      device/motorola/cedric/configs/mot_ov5695_chromatix.xml:system/etc/camera/mot_ov5695_chromatix.xml \
+                      device/motorola/cedric/configs/msm8937_mot_camera.xml:system/etc/camera/msm8937_mot_camera.xml
+
