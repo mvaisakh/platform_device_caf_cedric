303114943b0bea18c9863f4cd4653cb70d64fd15
Vaisakh Murali <vaisakhmurali@gmail.com>
2018-03-10 04:38:01 +0000

    cedric: Lunch targets
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo poly_cedric-userdebug
-add_lunch_combo poly_cedric-user
-add_lunch_combo poly_cedric-eng
+add_lunch_combo aosp_cedric-userdebug
+add_lunch_combo aosp_cedric-user
+add_lunch_combo aosp_cedric-eng
